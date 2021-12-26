9e3a0d3fc412743c02691a72ed1f005ffee73e65
Takumi <nase49946@gmail.com>
2021-12-26 18:34:31 +0900

    init

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+const http = require('http');
+
+const server = http
+  .createServer((req, res) => {
+    const now = new Date();
+    console.info(`[${now}] Requested by ${req.socket.remoteAddress}`);
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8',
+    });
+
+    switch (req.method) {
+      case 'GET':
+        const fs = require('fs');
+        const rs = fs.createReadStream('./form.html');
+        rs.pipe(res);
+        break;
+      case 'POST':
+        let rawData = '';
+        req
+          .on('data', (chunk) => {
+            rawData = rawData + chunk;
+          })
+          .on('end', () => {
+            const decoded = decodeURIComponent(rawData);
+            const qs = require('querystring');
+            const answer = qs.parse(decoded);
+            console.info(`[${now}] 投稿: ${decoded}`);
+            res.write(
+              '<!DOCTYPE html><html lang="ja"><body><h1>' +
+                `${answer['name']}さんは${answer['yaki-shabu']}に投票しました` +
+                '</h1></body></html>'
+            );
+            res.end();
+          });
+        break;
+      default:
+        break;
+    }
+  })
+  .on('error', (e) => {
+    console.error(`[${new Date()}] Server Error`, e);
+  })
+  .on('clientError', (e) => {
+    console.error(`[${new Date()}] Client Error`, e);
+  });
+const port = 8000;
+server.listen(port, () => {
+  console.log('Listening on ' + port);
+});
