4d766aed29b1737ee1b0a0e387454c9770ec3e39
Takumi <nase49946@gmail.com>
2021-12-27 10:49:28 +0900

    basic認証追加

--- index.js
+++ index.js
@@ -1,83 +1,102 @@
 'use strict';
 const http = require('http');
 const pug = require('pug');
+const auth = require('http-auth');
+
+const basic = auth.basic(
+  { realm: 'Enquetes Area.' },
+  (username, password, callback) => {
+    callback(username === 'guest' && password === 'xaXZJQmE');
+  }
+);
 
 const server = http
-  .createServer((req, res) => {
-    const now = new Date();
-    console.info(`[${now}] Requested by ${req.socket.remoteAddress}`);
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8',
-    });
+  .createServer(
+    basic.check((req, res) => {
+      const now = new Date();
+      console.info(`[${now}] Requested by ${req.socket.remoteAddress}`);
 
-    switch (req.method) {
-      case 'GET':
-        if (req.url === '/') {
-          res.write(
-            '<!DOCTYPE html><html lang="ja"><body>' +
-              '<h1>アンケートフォーム</h1>' +
-              '<a href="/enquetes">アンケート一覧</a>' +
-              '</body></html>'
-          );
-        } else if (req.url === '/enquetes') {
-          res.write(
-            '<!DOCTYPE html><html lang="ja"><body>' +
-              '<h1>アンケート一覧</h1><ul>' +
-              '<li><a href="/enquetes/yaki-shabu">焼き肉・しゃぶしゃぶ</a></li>' +
-              '<li><a href="/enquetes/rice-bread">ごはん・パン</a></li>' +
-              '<li><a href="/enquetes/sushi-pizza">寿司・ピザ</a></li>' +
-              '</ul></body></html>'
-          );
-        } else if (req.url === '/enquetes/yaki-shabu') {
-          res.write(
-            pug.renderFile('./form.pug', {
-              path: req.url,
-              firstItem: '焼肉',
-              secondItem: 'しゃぶしゃぶ',
-            })
-          );
-        } else if (req.url === '/enquetes/rice-bread') {
-          res.write(
-            pug.renderFile('./form.pug', {
-              path: req.url,
-              firstItem: 'ごはん',
-              secondItem: 'パン',
-            })
-          );
-        } else if (req.url === '/enquetes/sushi-pizza') {
-          res.write(
-            pug.renderFile('./form.pug', {
-              path: req.url,
-              firstItem: '寿司',
-              secondItem: 'ピザ',
-            })
-          );
-        }
-        res.end();
-        break;
-      case 'POST':
-        let rawData = '';
-        req
-          .on('data', (chunk) => {
-            rawData = rawData + chunk;
-          })
-          .on('end', () => {
-            const decoded = decodeURIComponent(rawData);
-            const qs = require('querystring');
-            const answer = qs.parse(decoded);
-            console.info(`[${now}] 投稿: ${decoded}`);
+      if (req.url === '/logout') {
+        res.writeHead(401, {
+          'Content-Type': 'text/plain; charset=utf-8',
+        });
+        res.end('ログアウトしました');
+        return;
+      }
+
+      res.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8',
+      });
+
+      switch (req.method) {
+        case 'GET':
+          if (req.url === '/') {
             res.write(
-              '<!DOCTYPE html><html lang="ja"><body><h1>' +
-                `${answer['name']}さんは${answer['favorite']}に投票しました` +
-                '</h1></body></html>'
+              '<!DOCTYPE html><html lang="ja"><body>' +
+                '<h1>アンケートフォーム</h1>' +
+                '<a href="/enquetes">アンケート一覧</a>' +
+                '</body></html>'
             );
-            res.end();
-          });
-        break;
-      default:
-        break;
-    }
-  })
+          } else if (req.url === '/enquetes') {
+            res.write(
+              '<!DOCTYPE html><html lang="ja"><body>' +
+                '<h1>アンケート一覧</h1><ul>' +
+                '<li><a href="/enquetes/yaki-shabu">焼き肉・しゃぶしゃぶ</a></li>' +
+                '<li><a href="/enquetes/rice-bread">ごはん・パン</a></li>' +
+                '<li><a href="/enquetes/sushi-pizza">寿司・ピザ</a></li>' +
+                '</ul></body></html>'
+            );
+          } else if (req.url === '/enquetes/yaki-shabu') {
+            res.write(
+              pug.renderFile('./form.pug', {
+                path: req.url,
+                firstItem: '焼肉',
+                secondItem: 'しゃぶしゃぶ',
+              })
+            );
+          } else if (req.url === '/enquetes/rice-bread') {
+            res.write(
+              pug.renderFile('./form.pug', {
+                path: req.url,
+                firstItem: 'ごはん',
+                secondItem: 'パン',
+              })
+            );
+          } else if (req.url === '/enquetes/sushi-pizza') {
+            res.write(
+              pug.renderFile('./form.pug', {
+                path: req.url,
+                firstItem: '寿司',
+                secondItem: 'ピザ',
+              })
+            );
+          }
+          res.end();
+          break;
+        case 'POST':
+          let rawData = '';
+          req
+            .on('data', (chunk) => {
+              rawData = rawData + chunk;
+            })
+            .on('end', () => {
+              const decoded = decodeURIComponent(rawData);
+              const qs = require('querystring');
+              const answer = qs.parse(decoded);
+              console.info(`[${now}] 投稿: ${decoded}`);
+              res.write(
+                '<!DOCTYPE html><html lang="ja"><body><h1>' +
+                  `${answer['name']}さんは${answer['favorite']}に投票しました` +
+                  '</h1></body></html>'
+              );
+              res.end();
+            });
+          break;
+        default:
+          break;
+      }
+    })
+  )
   .on('error', (e) => {
     console.error(`[${new Date()}] Server Error`, e);
   })
